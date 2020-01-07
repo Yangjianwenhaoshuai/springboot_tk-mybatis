fac849654f992ababaeeb4ea227f441d27b6373a
Yangjianwenhaoshuai <2636526837@qq.com>
2020-01-07 10:26:22 +0800

    代码生成+mk_mybatis

---
+++ src/main/java/com/yjw/spring/boot/mybatis/demo/SpringBootMybatisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yjw.spring.boot.mybatis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjw/spring/boot/mybatis/demo/SpringBootMybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjw.spring.boot.mybatis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
